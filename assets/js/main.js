7139b33422b5bb429a9e9461851dc8c1577061e6
EGM17 <86752287+EGM17@users.noreply.github.com>
2022-01-27 04:11:20 -0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,155 @@
+/*===== MENU SHOW Y HIDDEN =====*/ 
+const navMenu = document.getElementById('nav-menu'),
+      toggleMenu = document.getElementById('nav-toggle'),
+      closeMenu = document.getElementById('nav-close')
+
+/*SHOW*/ 
+toggleMenu.addEventListener('click', ()=>{
+    navMenu.classList.toggle('show')
+})
+
+/*HIDDEN*/
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show')
+})
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+  /*Active link*/
+  navLink.forEach(n => n.classList.remove('active'));
+  this.classList.add('active');
+  
+  /*Remove menu mobile*/
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+/*===== READ MORE BUTTON =====*/
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Leer m\u00E1s";
+      moreText.style.display = "none";
+  } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Leer menos";
+      moreText.style.display = "inline";
+  }
+}
+
+/*===== PROGRESS BAR RESPONSIVE =====*/
+// progressbar.js@1.0.0 version is used
+// Docs: http://progressbarjs.readthedocs.org/en/1.0.0/
+
+var bar = new ProgressBar.Circle(container, {
+  color: '#aaa',
+  // This has to be the same size as the maximum width to
+  // prevent clipping
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: false
+  },
+  from: { color: '#070D1F', width: 1 },
+  to: { color: '#070D1F', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    var value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontSize = '1.5rem';
+bar.text.style.fontWeight = '600';
+bar.text.style.color = '#070D1F';
+
+bar.animate(1);  // Number from 0.0 to 1.0
+
+
+var bar2 = new ProgressBar.Circle(containertwo, {
+  color: '#aaa',
+  // This has to be the same size as the maximum width to
+  // prevent clipping
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: false
+  },
+  from: { color: '#070D1F', width: 1 },
+  to: { color: '#070D1F', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    var value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+bar2.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar2.text.style.fontSize = '1.5rem';
+bar2.text.style.fontWeight = '600';
+bar2.text.style.color = '#070D1F';
+
+bar2.animate(0.8);  // Number from 0.0 to 1.0
+
+
+
+
+
+var bar3 = new ProgressBar.Circle(containertres, {
+  color: '#aaa',
+  // This has to be the same size as the maximum width to
+  // prevent clipping
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: false
+  },
+  from: { color: '#070D1F', width: 1 },
+  to: { color: '#070D1F', width: 4 },
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    var value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+bar3.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar3.text.style.fontSize = '1.5rem';
+bar3.text.style.fontWeight = '600';
+bar3.text.style.color = '#070D1F';
+
+bar3.animate(1);  // Number from 0.0 to 1.0
